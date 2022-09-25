b83dc67c16d00863b07b95050849f9882f38d32f
eca-kubi <ecakubi@gmail.com>
2022-09-25 02:16:42 +0000

    Auth setup successfully.

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'ecakubi.us.auth0.com', // the auth0 domain prefix
+    url: 'ecakubi.us', // the auth0 domain prefix
     audience: 'https://coffeeshop.softenprise.com/api/v2/', // the audience set for the auth0 app
     clientId: '9xO9qDfNZiaqFmQ5ZpKWOarm8SEnm5Xu', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
